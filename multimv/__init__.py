d9f1ae453bf15975e3de710ad3234a61be6158d5
Summer <30243134+Summertime@users.noreply.github.com>
2021-07-28 07:59:08 +0930

    Rewrite

--- multimv/__init__.py
+++ multimv/__init__.py
@@ -1,55 +1,70 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser, Namespace
+from re import compile
+from types import SimpleNamespace
 from pathlib import Path
 import shlex
+import os.path
+from pprint import pprint
+from functools import reduce
+import click
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("-m", "--method", choices=["re"], default="re")
-    parser.add_argument("-g", "--global", dest="global_", action="store_true")
-    parser.add_argument("-i", "--ignore-case", action="store_true")
-    parser.add_argument("-d", "--dry-run", action="store_true")
-    parser.add_argument("-q", "--quiet", action="store_true")
-    parser.add_argument("pattern")
-    parser.add_argument("replacement")
-    parser.add_argument("files", nargs="*", type=Path)
-    args = parser.parse_args()
+from .vendor.toposort import toposort
 
-
-    # Per method code
-    if args.method == "re":
-        import re
-
-        def new_path(old):
-            return old.with_name(
-                re.sub(
-                    args.pattern,
-                    repl=args.replacement,
-                    string=old.name,
-                    flags=(re.I if args.ignore_case else 0),
-                    count=(0 if args.global_ else 1),
-                )
-            )
+@click.group()
+@click.option("-n", "--dry-run", is_flag=True)  # -n is more common
+@click.pass_context
+def main(ctx, dry_run):
+    ctx.ensure_object(SimpleNamespace)
+    ctx.obj.dry_run = dry_run
 
 
-    # Generic code
-    moves = []
-    for old in args.files:
-        if old != (new := new_path(old)):
-            moves.append(Namespace(old=old, new=new))
+@main.command()
+@click.option("-g", "--global", "global_", is_flag=True)
+@click.option("--eval", "eval_", is_flag=True)
+@click.argument("pattern")
+@click.argument("replacement")
+@click.argument("files", nargs=-1, type=click.Path())
+@click.pass_context
+def re(ctx, pattern, replacement, files, eval_, global_):
+    if eval_:
+        repl = replacement
+        def replacement(match):
+            local = {
+                "m0": match.group(0),
+                **{f"m{i}": g for i, g in enumerate(match.groups(), 1)},
+                **match.groupdict(),
+            }
+            return str(eval(repl, {}, local))
 
-    if any(m.new.exists() for m in moves):
-        raise Error  # Collides with pre-existing file
+    count = 0 if global_ else 1
+    regex = compile(pattern)
+    moves = {f: regex.sub(replacement, f, count=count) for f in files}
+    perform_moves(ctx, moves)
 
-    if len({m.new for m in moves}) < len(moves):
-        raise Error  # List has duplicates
 
-    for move in moves:
-        if not args.quiet:
-            print(
-                f"- {shlex.quote(str(move.old))}",
-                f"+ {shlex.quote(str(move.new))}",
-                sep="\n",
-            )
-        if not args.dry_run:
-            move.old.rename(move.new)
+def perform_moves(ctx, moves):
+    unmoved = {k for k,v in moves.items() if k == v or v is None}
+    if len(l := list(moves.keys())) != len(sources := set(l)):
+        raise Exception('Duplicate sources')
+    if len(l := list(moves.values())) != len(targets := set(l)):
+        raise Exception('Duplicate destinations')
+    if not ctx.obj.dry_run:
+        if any(not os.path.exists(f) for f in sources - unmoved):
+            raise Exception('Non-existant source')
+        if any(os.path.exists(f) for f in targets - sources - unmoved):
+            raise Exception('Pre-existing destination')
+    topo = {k:{v} for k,v in moves.items() if k not in unmoved}
+    changes = {k:next(iter(topo[k])) for ks in toposort(topo) for k in ks}
+    for u in unmoved:
+        print(f"~ {shlex.quote(u)}")
+        
+    for src,tgt in changes.items():
+        print(
+            f"- {shlex.quote(src)}",
+            f"+ {shlex.quote(tgt)}",
+            sep="\n",
+        )
+        if not ctx.obj.dry_run:
+            os.rename(src,tgt)
+if __name__ == "__main__":
+    main()
